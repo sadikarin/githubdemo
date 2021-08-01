df7603ef63964304fb9f196977643ecc461877ee
sadikarin <sadikarin@gmail.com>
2021-08-01 22:40:52 +0300

    second commit

--- src/Package1/ClassOne.java
+++ src/Package1/ClassOne.java
@@ -5,6 +5,7 @@ public class ClassOne {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Batch 35 / 36 learn Github");
     }
 
 
